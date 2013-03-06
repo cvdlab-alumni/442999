813a40a5af047cb06dd41d7393498500fac0d2b6
farruggio <farruggioandrea@gmail.com>
2013-03-06 19:57:47 +0100

    esercizi

---
+++ learning-java/2013-03-06/exercise03.js
@@ -0,0 +1,17 @@
+var s = '\n';
+var r = 10;
+var c = 10;
+
+for (var i = 0; i <=r; i++) {
+  for (var j = 0; j <= c; j++) {
+  	
+
+    if (i==j){if(j===c) s += 1 ; else s+='1, ';}
+
+    	 else {if(j===c) s += 0; else s+='0, '};
+
+  
+  }
+  s += '\n';
+}
+console.log(s);
